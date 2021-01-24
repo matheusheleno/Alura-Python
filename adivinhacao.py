a75331d726115c1efc9584d379283a8bf6c154cc
matheusheleno16 <matheusheleno16@gmail.com>
2021-01-24 20:35:49 -0300

    30% curso Python inicio

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,24 +1,26 @@
 print("--------------------------------------------------")
 print('*        Bem vindo ao jogo de adivinhação        *')
 print(' Chute um numero de 0 a 10, você tem 5 tentativas ')
+print(' ================= BOA SORTE ==================== ')
 print("--------------------------------------------------")
-
+# --------variaveis--------- #
 numero_secreto = 9
-
+chute = 0
 tentativas = 5
+# --------CODE--------- #
+while tentativas > 0 and chute != numero_secreto:
+    chute = input("Digite um numero: ")
+    chute = int(chute)
+    print("Você digitou: ", chute)
+    if numero_secreto == chute:
+        print("Parabéns, Você acertou!")
+    else:
+        tentativas = tentativas - 1
+        if tentativas == 0:
+            print("Você errou, acabaram suas tentativas :/ !")
+        else:
+            print("Você errou, tente novamente, você ainda tem ", tentativas, "Tentativas!")
 
-chute = input("Digite um numero: ")
+# Fim do While
 
-# if (tentativas > 0 or chute != numero_secreto )
-
-# chute = input("Digite um numero: ")
-
-print("Você digitou: ",chute)
-
-if (numero_secreto == chute):
-    print("Parabéns, Você acertou!")
-else:
-    tentativas = tentativas - 1
-    print("Você errou, tente novamente")
-
-#fim do código.
+# Fim do código.
