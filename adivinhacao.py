22b71e7113e6119155a2955959ec1c6137e226b7
matheusheleno16 <matheusheleno16@gmail.com>
2021-03-03 17:22:30 -0300

    Submited

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,29 +1,41 @@
+import random
+
 print("--------------------------------------------------")
 print('*        Bem vindo ao jogo de adivinhação        *')
-print(' Chute um numero de 0 a 10, você tem 5 tentativas ')
+print(' Chute um numero de 0 a 9, você tem 5 tentativas  ')
 print(' ================= BOA SORTE ==================== ')
 print("--------------------------------------------------")
 # --------variaveis--------- #
-numero_secreto = 9
+numero_secreto = (random.randint(0, 9))
 chute = 0
 tentativas = 5
 # --------CODE--------- #
 while tentativas > 0 and chute != numero_secreto:
+    # ---- VAR TEMP ---- #
     chute = input("Digite um numero: ")
     chute = int(chute)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+    fim_tenta = tentativas == 0
+    valido = 10 > chute >= 0
+    # ---- VAR TEMP ---- #
     print("Você digitou: ", chute)
-    if numero_secreto == chute:
-        print("Parabéns, Você acertou!")
-    else:
-        tentativas = tentativas - 1
-        if tentativas == 0:
-            print("Você errou, acabaram suas tentativas :/ !")
+    if valido:
+        if acertou:
+            print("Parabéns, Você acertou!")
         else:
-            print("Errrrou!! Numero de Tentativas: ", tentativas)
-            if chute > numero_secreto:
-                print("Dica: Abaixa um pouco a bola ai campeão, ta alto demais")
-            if chute < numero_secreto:
-                print("Dica: Um pouco mais! Um pouco mais Leonidas?!")
+            tentativas = tentativas - 1
+            if fim_tenta:
+                print("!! YOU LOSE - O NUMERO ERA: ", numero_secreto, " ATE A PROXIMA !!")
+            else:
+                print("Errrrou!! Numero de Tentativas: ", tentativas)
+                if maior:
+                    print("Dica: Abaixa um pouco a bola ai campeão, ta alto demais!")
+                if menor:
+                    print("Dica: Um pouco mais! Um pouco mais Leonidas?!")
+    else:
+        print("O valor", chute, " é invalido informe um valor de 0 a 9!")
 # Fim do While
 
 # Fim do código.
